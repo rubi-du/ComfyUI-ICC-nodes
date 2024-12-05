ad9db9783e81bf71aef90b25cc7c94fb31ce7170
rubi-du <duhaifeng1@126.com>
2024-12-05 15:54:44 +0800

    feat: init nodes

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# ComfyUI-ICC-nodes
+<h1 align="center">ComfyUI-ICC-nodes </h1>  
+
+  
+## Introduction  
 This repository support processing Comfyui image nodes with ICC profile, load and save images with ICC profile
+
+#### Method 1:  
+  
+1. Navigate to the node directory, `ComfyUI/custom_nodes/`  
+2. `git clone https://github.com/rubi-du/ComfyUI-ICC-nodes.git`  
+3. Restart ComfyUI  
+  
+#### Method 2:  
+Directly download the node source code package, unzip it into the `custom_nodes` directory, and then restart ComfyUI.  
+  
+#### Method 3:  
+Install via ComfyUI-Manager by searching for "ComfyUI-ICC-nodes". 
+
+
+## Usage  
+### Nodes
+- LoadImageICC
+- SaveImageICC
+- PreviewImageICC
+
+
+### Workflows 
+Example workflows are placed in `ComfyUI-ICC-nodes/workflow`.
+[Workflow Address](./workflow/image.json) 
+ 
+workflow:
+![plot](./assets/image.png) 
+
+test image: ![test image](./assets/test_icc.png) 
---
+++ nodes.py
@@ -0,0 +1,162 @@
+
+import os
+import json
+
+import numpy as np
+import random
+import torch
+from PIL import Image, ImageSequence, ImageOps
+from PIL.PngImagePlugin import PngInfo
+
+import folder_paths
+import node_helpers
+from comfy.cli_args import args
+
+class LoadImageICC:
+    @classmethod
+    def INPUT_TYPES(s):
+        input_dir = folder_paths.get_input_directory()
+        files = [f for f in os.listdir(input_dir) if os.path.isfile(os.path.join(input_dir, f))]
+        return {"required":
+                    {"image": (sorted(files), {"image_upload": True})},
+                }
+
+    CATEGORY = "image"
+
+    RETURN_TYPES = ("IMAGE", "MASK", "ICC_PROFILE")
+    FUNCTION = "load_image"
+    def load_image(self, image):
+        image_path = folder_paths.get_annotated_filepath(image)
+        
+        img = node_helpers.pillow(Image.open, image_path)
+        
+        output_images = []
+        output_masks = []
+        w, h = None, None
+
+        excluded_formats = ['MPO']
+        
+        for i in ImageSequence.Iterator(img):
+            i = node_helpers.pillow(ImageOps.exif_transpose, i)
+
+            if i.mode == 'I':
+                i = i.point(lambda i: i * (1 / 255))
+            image = i.convert("RGB")
+
+            if len(output_images) == 0:
+                w = image.size[0]
+                h = image.size[1]
+            
+            if image.size[0] != w or image.size[1] != h:
+                continue
+            
+            image = np.array(image).astype(np.float32) / 255.0
+            image = torch.from_numpy(image)[None,]
+            if 'A' in i.getbands():
+                mask = np.array(i.getchannel('A')).astype(np.float32) / 255.0
+                mask = 1. - torch.from_numpy(mask)
+            else:
+                mask = torch.zeros((64,64), dtype=torch.float32, device="cpu")
+            output_images.append(image)
+            output_masks.append(mask.unsqueeze(0))
+
+        if len(output_images) > 1 and img.format not in excluded_formats:
+            output_image = torch.cat(output_images, dim=0)
+            output_mask = torch.cat(output_masks, dim=0)
+        else:
+            output_image = output_images[0]
+            output_mask = output_masks[0]
+
+        return (output_image, output_mask, img.info.get('icc_profile', None))
+    
+    
+class SaveImageICC:
+    def __init__(self):
+        self.output_dir = folder_paths.get_output_directory()
+        self.type = "output"
+        self.prefix_append = ""
+        self.compress_level = 4
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "images": ("IMAGE", {"tooltip": "The images to save."}),
+                "icc_profile": ("ICC_PROFILE", {"tooltip": "The ICC profile to save."}),
+                "filename_prefix": ("STRING", {"default": "ComfyUI", "tooltip": "The prefix for the file to save. This may include formatting information such as %date:yyyy-MM-dd% or %Empty Latent Image.width% to include values from nodes."})
+            },
+            "hidden": {
+                "prompt": "PROMPT", "extra_pnginfo": "EXTRA_PNGINFO"
+            },
+        }
+
+    RETURN_TYPES = ()
+    FUNCTION = "save_images"
+
+    OUTPUT_NODE = True
+
+    CATEGORY = "image"
+    DESCRIPTION = "Saves the input images to your ComfyUI output directory."
+
+    def save_images(self, images, icc_profile=None, filename_prefix="ComfyUI", prompt=None, extra_pnginfo=None):
+        filename_prefix += self.prefix_append
+        full_output_folder, filename, counter, subfolder, filename_prefix = folder_paths.get_save_image_path(filename_prefix, self.output_dir, images[0].shape[1], images[0].shape[0])
+        results = list()
+        extra_save_args = {}
+        if icc_profile is not None:
+            extra_save_args["icc_profile"] = icc_profile
+        for (batch_number, image) in enumerate(images):
+            i = 255. * image.cpu().numpy()
+            img = Image.fromarray(np.clip(i, 0, 255).astype(np.uint8))
+            metadata = None
+            if not args.disable_metadata:
+                metadata = PngInfo()
+                if prompt is not None:
+                    metadata.add_text("prompt", json.dumps(prompt))
+                if extra_pnginfo is not None:
+                    for x in extra_pnginfo:
+                        metadata.add_text(x, json.dumps(extra_pnginfo[x]))
+    
+            filename_with_batch_num = filename.replace("%batch_num%", str(batch_number))
+            file = f"{filename_with_batch_num}_{counter:05}_.png"
+            img.save(os.path.join(full_output_folder, file), pnginfo=metadata, compress_level=self.compress_level, **extra_save_args)
+            results.append({
+                "filename": file,
+                "subfolder": subfolder,
+                "type": self.type
+            })
+            counter += 1
+
+        return { "ui": { "images": results } }
+    
+    
+class PreviewImageICC(SaveImageICC):
+    def __init__(self):
+        self.output_dir = folder_paths.get_temp_directory()
+        self.type = "temp"
+        self.prefix_append = "_temp_" + ''.join(random.choice("abcdefghijklmnopqrstupvxyz") for x in range(5))
+        self.compress_level = 1
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+                "required":{
+                    "images": ("IMAGE", ),
+                    "icc_profile": ("ICC_PROFILE", {"tooltip": "The ICC profile to save."}),
+                },
+                "hidden": {"prompt": "PROMPT", "extra_pnginfo": "EXTRA_PNGINFO"},
+            }
+        
+        
+NODE_CLASS_MAPPINGS = {
+    "LoadImageICC": LoadImageICC,
+    "SaveImageICC": SaveImageICC,
+    "PreviewImageICC": PreviewImageICC,
+}
+
+# A dictionary that contains the friendly/humanly readable titles for the nodes
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "LoadImageICC": "Load Image ICC",
+    "SaveImageICC": "Save Image ICC",
+    "PreviewImageICC": "Preview Image ICC",
+}
